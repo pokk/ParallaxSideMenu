3487162455e75a890d8499608fc601ec04c43a96
Jieyi <pokkbaby@gmail.com>
2018-02-08 01:39:04 +0900

    Added the parallax side menu module.

--- sample/src/main/java/com/devrapid/sample/MainActivity.kt
+++ sample/src/main/java/com/devrapid/sample/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.devrapid.parallaxsidemenu
+package com.devrapid.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.devrapid.parallaxsidemenu.R
 
 class MainActivity : AppCompatActivity() {
 
